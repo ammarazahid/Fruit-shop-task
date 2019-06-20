b338f60af52c496acdd5f05623a1ba8560212332
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-06-20 10:41:42 +0500

    #wip js

---
+++ script.js
@@ -0,0 +1,46 @@
+let appleCost = 0;
+
+function applePlusBtn() {
+   appleCost += 10;
+   document.querySelector("#price-btn1").innerHTML = appleCost;       
+
+}
+
+function appleMinusBtn() {
+    appleCost -= 10;
+    document.querySelector("#price-btn1").innerHTML = appleCost;       
+ 
+ 
+ }
+
+
+ let orangeCost = 0;
+
+function orangePlusBtn() {
+   orangeCost += 15;
+   document.querySelector("#price-btn2").innerHTML = orangeCost;       
+
+}
+
+function orangeMinusBtn() {
+    orangeCost -= 15;
+    document.querySelector("#price-btn2").innerHTML = orangeCost;       
+ 
+ 
+ }
+
+
+ let bananaCost = 0;
+
+function bananaPlusBtn() {
+   bananaCost += 7;
+   document.querySelector("#price-btn3").innerHTML = bananaCost;       
+
+}
+
+function bananaMinusBtn() {
+    bananaCost -= 7;
+    document.querySelector("#price-btn3").innerHTML = bananaCost;       
+ 
+ 
+ }
